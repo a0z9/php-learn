190e1f90637a06f0f924ae852979c20b79f89a46
Sand <a0z9@rambler.ru>
2023-03-12 09:55:49 +0800

    classes - interface

--- Classes/rocket-test.php
+++ Classes/rocket-test.php
@@ -1,9 +1,16 @@
 <?php
-require "rockets/baserocket.php";
-require "rockets/heavy-rocket.php";
+require_once "rockets/baserocket.php";
+require_once "rockets/heavy-rocket.php";
 
 use istu\rockets\BaseRocket;
 use istu\rockets\HeavyRocket;
+use istu\rockets\Ignitable;
+
+function testIgnit(Ignitable $i)
+{
+  $i->ignit();
+}
+
 
 $rocket= new BaseRocket;
 $rocket->ignit();
@@ -13,3 +20,6 @@ echo $rocket . "\n";
 $rocket= new HeavyRocket;
 $rocket->ignit();
 echo $rocket . "\n";
+
+echo "------------------------------------\n";
+for($i=0;$i<5;$i++) testIgnit(new HeavyRocket);
