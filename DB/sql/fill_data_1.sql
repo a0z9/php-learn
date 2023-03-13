49a37389fc1dedd55a0b0d26ce23ed8a3f152559
Sand <a0z9@rambler.ru>
2023-03-13 11:04:04 +0800

    tran test

--- DB/sql/fill_data_1.sql
+++ DB/sql/fill_data_1.sql
@@ -1,12 +1,36 @@
 use customers;
 
 insert into users(name,sname,expired)
-values("Andrei","The Admin", date_add(now(),interval 7 day));
+values("Kate","The Student", date_add(now(),interval 7 day));
 
 select * from users;
 
 insert into logins(user_id,login,password,expires)
-values (1,"basil2",sha2("pass",256), date_add(now(),interval 15 day)) 
+values (3,"kate",sha2("pass",256), date_add(now(),interval 15 day)); 
+
+---- transaction test -------
+start transaction;
+
+insert into users(name,sname,expired)
+values("Jane","The Student", date_add(now(),interval 7 day));
+
+set @id = last_insert_id();
+select @id as 'imserted id';
+insert into logins(user_id,login,password,expires)
+values (@id,"jane",sha2("pass",256), date_add(now(),interval 15 day)); 
+
+commit;
 
 
 select * from logins;
+
+--- old style
+select name, sname, login
+from logins, users
+where logins.user_id = users.id;
+
+-- join style
+select u.name, u.sname, l.login
+from logins l
+join users u
+on l.user_id = u.id;
