0a635d3c954a4105a2202f36aac77f691fac367f
Sand <a0z9@rambler.ru>
2023-03-13 10:00:50 +0800

    create table logins

--- DB/sql/create_customers_tables.sql
+++ DB/sql/create_customers_tables.sql
@@ -5,4 +5,17 @@ create table if not exists users
  name varchar(64) not null,
  sname varchar(64) not null,
  expired date
-)
+);
+
+create table if not exists logins
+(id int auto_increment,
+ user_id int not null,
+ login varchar(64) not null,
+ password varchar(64) not null,
+ expires date,
+ is_locked boolean not null default 0,
+ constraint foreign key login_user_id (user_id) references users(id),
+ primary key (id)
+
+);
+
